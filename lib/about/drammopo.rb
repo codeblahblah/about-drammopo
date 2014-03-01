5b33cd2ddee64022709a7551a1bc3a3253378827
drammopo <drammopo@gmail.com>
2014-03-01 13:16:07 +0200

    Initial commit

---
+++ lib/about/drammopo.rb
@@ -0,0 +1,7 @@
+require "about/drammopo/version"
+
+module About
+  module Drammopo
+    # Your code goes here...
+  end
+end
---
+++ lib/about/drammopo/version.rb
@@ -0,0 +1,5 @@
+module About
+  module Drammopo
+    VERSION = "0.0.1"
+  end
+end
